3f9d71db8622a2b22601ddf22c7b2966521212f1
dlozon <lozo6089@kettering.edu>
2022-01-30 11:50:57 -0500

    restored changes deleted by merge

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -1,10 +1,16 @@
 package frc.robot.subsystems;
 
+// Imports
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class ShuffBoard {
-    public ShuffBoard() {}
+// This object is used to handle SmartDashboard outputs
+public class Dashboard {
+    // Constructor method
+    public Dashboard() {}
 
+    // Prints the rpm of a shooter object
+    // Shooters have two motors, but we only need to print the RPM
+    // of one motor, because they're locked together physically on the robot
     public void printShooterRPM( Shooter shooter ) {
         SmartDashboard.putNumber("ShooterRPM", Math.round(shooter.getBottom().getEncoder().getVelocity()));
     }
