06c0f9cea141a21e9e1b27d4c8e9438e5e1e8955
dlozon <lozo6089@kettering.edu>
2022-03-09 12:54:08 -0500

    reworked climber to check limit switches in-class

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import frc.robot.wrappers.LimitSwitch;
 
 // Controls the climber
 public class Climber {
@@ -9,27 +10,44 @@ public class Climber {
     // Declare motors
     private CANSparkMax leftMotor;
     private CANSparkMax rightMotor;
+    
+    // Declare limit switches
+    private LimitSwitch leftLimitSwitch;
+    private LimitSwitch rightLimitSwitch;
 
     // Constructor
-    public Climber (int motorID1, int motorID2) {
-        
+    public Climber (int motorID1, int motorID2, int leftLimitSwitchID, int rightLimitSwitchID) {
         // Initialize motors
         leftMotor = new CANSparkMax(motorID1, MotorType.kBrushless);
         rightMotor = new CANSparkMax(motorID2, MotorType.kBrushless);
+
+        leftLimitSwitch = new LimitSwitch(leftLimitSwitchID);
+        rightLimitSwitch = new LimitSwitch(rightLimitSwitchID);
     }
 
     // Accessor methods
     public double getLeft() { return leftMotor.get(); }
     public double getRight() { return rightMotor.get(); }
 
-    // Set either of the arms separately
-    public void setLeft(double pwr) { leftMotor.set(pwr); }
-    public void setRight(double pwr) { rightMotor.set(pwr); }
+    // Set left arm's power
+    public void setLeft(double pwr) { 
+        leftMotor.set(
+            leftLimitSwitch.isPressed() ? pwr : Math.abs(pwr));
+    }
 
-    // Set the power of the motors
+    // Set right arm's power
+    public void setRight(double pwr) { 
+        rightMotor.set(
+            rightLimitSwitch.isPressed() ? pwr : Math.abs(pwr));
+    }
+
+    // Set the power of the motors together
     public void set(double pwr) {
-        leftMotor.set(pwr);
-        rightMotor.set(pwr);
+        leftMotor.set(
+            leftLimitSwitch.isPressed() ? pwr : Math.abs(pwr));
+
+        rightMotor.set(
+            rightLimitSwitch.isPressed() ? pwr : Math.abs(pwr));
     }
 
     // Stop the climber
